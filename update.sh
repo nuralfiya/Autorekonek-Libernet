a9c9897b5d823c820822d51f2e8e503b57dba57b
nuralfiya <95646157+nuralfiya@users.noreply.github.com>
2022-02-18 04:22:39 -0800

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#-- colors v2 --#
+R='\e[31;1m' #RED
+G='\e[32;1m' #GREEN
+Y='\e[33;1m' #YELLOW
+DB='\e[34;1m' #DARKBLUE
+P='\e[35;1m' #PURPLE
+LB='\e[36;1m' #LIGHTBLUE
+
+#-- colors v3 --#
+BR='\e[3;31m' #RED
+BG='\e[3;32m' #GREEN
+BY='\e[3;33m' #YELLOW
+BDB='\e[3;34m' #DARKBLUE
+BP='\e[3;35m' #PURPLE
+BLB='\e[3;36m' #LIGHTBLUE
+
+clear
+
+echo -e "$G Lagi gak ada Update gaesss"
