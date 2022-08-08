07d524c35410a3a530a442e8e02271709d933e8f
nuralfiya <95646157+nuralfiya@users.noreply.github.com>
2022-08-08 09:28:00 +0700

    Create anti-ddos.sh

---
+++ anti-ddos.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+sudo apt install dnsutils
+sudo apt-get install net-tools
+sudo apt-get install tcpdump
+sudo apt-get install dsniff -y
+sudo apt install grepcidr
+wget https://github.com/jgmdev/ddos-deflate/archive/master.zip -O ddos.zip
+unzip ddos.zip
+cd ddos-deflate-master
+./install.sh
+cd
+wget https://raw.githubusercontent.com/nuralfiya/Autorekonek-Libernet/main/ddos.sh
+wget https://raw.githubusercontent.com/nuralfiya/Autorekonek-Libernet/main/ddos.conf
+cp ddos.ssh /usr/local/ddos/ddos.sh
+chmod +x /usr/local/ddos/ddos.sh
+cp ddos.conf /etc/ddos/ddos.conf
+echo "sukses"
+reboot
